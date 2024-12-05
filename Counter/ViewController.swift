055f9dc2f93ff81ceab4acf268e252ca6a269c9e
DiK <korolevadidan@gmail.com>
2024-12-05 22:39:34 +0400

    Добавлена базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterValue: UILabel!
+    @IBOutlet weak var button: UIButton!
+    private var currentNumber: Int = 0
+    @IBAction func counterButton(_ sender: UIButton) {
+        currentNumber += 1
+        counterValue.text = "Значение счётчика: \(currentNumber)"
 
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        button.backgroundColor = .purple
+        button.layer.cornerRadius = 30
+
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    //    func changingValue(_ sender: Any)) {
+    //        if //клиент нажал на кнопку =>
+    //        counterValue += 1
+    //    }
 }
-
