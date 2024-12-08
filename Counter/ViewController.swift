3293de13d249a73edbbb2922f1e5cd8d535fb24e
DiK <korolevadidan@gmail.com>
2024-12-08 21:32:46 +0400

    Добавлены правки после review

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,33 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var counterValue: UILabel!
-    @IBOutlet weak var buttonPlus: UIButton!
-    @IBOutlet weak var buttonMinus: UIButton!
-    @IBOutlet weak var buttonClean: UIButton!
-    @IBOutlet weak var historyArea: UITextView!
+    @IBOutlet private weak var counterValueLabel: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var cleanButton: UIButton!
+    @IBOutlet private weak var historyTextView: UITextView!
     private var currentNumber: Int = 0
-    var historyTitle = "История изменений:"
-    var historyNewNote: [String] = []
-    var formDate: String {
+    private var historyTitle = "История изменений:"
+    private var historyNewNote: [String] = []
+    private var formDate: String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
         let currentDate = Date()
         return dateFormatter.string(from: currentDate)
     }
   
- 
-    @IBAction func increaseButton(_ sender: UIButton) {
+    @IBAction private func increaseButton(_ sender: UIButton) {
         currentNumber += 1
-        counterValue.text = "Значение счётчика: \(currentNumber)"
+        counterValueLabel.text = "Значение счётчика: \(currentNumber)"
         historyNewNote.append("\(formDate): значение изменено на +1")
         updatehistoryArea()
     }
     
-    @IBAction func decreaseButton(_ sender: UIButton) {
+    @IBAction private func decreaseButton(_ sender: UIButton) {
         currentNumber -= 1
         if currentNumber >= 0 {
-            counterValue.text = "Значение счётчика: \(currentNumber)"
+            counterValueLabel.text = "Значение счётчика: \(currentNumber)"
             historyNewNote.append("\(formDate): значение изменено на -1")
             updatehistoryArea()
         }
@@ -46,40 +45,30 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func clearButton(_ sender: UIButton) {
+    @IBAction private func clearButton(_ sender: UIButton) {
         currentNumber = 0
-        counterValue.text = "Значение счётчика: \(currentNumber)"
-//        historyNewNote.removeAll()
+        counterValueLabel.text = "Значение счётчика: \(currentNumber)"
         historyNewNote.append("\(formDate): значение сброшено")
         updatehistoryArea()
     }
     
-//    @IBAction func addHistory(_ sender: UITextField) {
-//    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        buttonPlus.backgroundColor = .red
-        buttonPlus.layer.cornerRadius = 30
+        plusButton.backgroundColor = .red
+        plusButton.layer.cornerRadius = 30
         
-        buttonMinus.backgroundColor = .blue
-        buttonMinus.layer.cornerRadius = 30
+        minusButton.backgroundColor = .blue
+        minusButton.layer.cornerRadius = 30
         
         let buttonImage = UIImage(systemName: "trash")
-        buttonClean.setImage(buttonImage, for: .normal)
-        historyArea.isEditable = false
+        cleanButton.setImage(buttonImage, for: .normal)
+        historyTextView.isEditable = false
         
         updatehistoryArea()
-
-        // Do any additional setup after loading the view.
     }
-    func updatehistoryArea() {
+    private func updatehistoryArea() {
         let fullHistory = historyTitle + "\n\n" + historyNewNote.joined(separator: "\n")
-        historyArea.text = fullHistory
+        historyTextView.text = fullHistory
     }
-    
-    
-    //    func changingValue(_ sender: Any)) {
-    //        if //клиент нажал на кнопку =>
-    //        counterValue += 1
-    //    }
+
 }
